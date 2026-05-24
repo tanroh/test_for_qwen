91baef4f06f1886d9a0ca6c56a8535b9f1656a81
Rohan Tankey <156636557+tanroh@users.noreply.github.com>
2026-05-24 23:35:24 +1000

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -27,7 +27,7 @@ preset_locations = {
 }
 
 # API configuration
-API_KEY = '9106e00acbe0c72f0d04e9f21c4bf5c8'
+API_KEY = "9106e00acbe0c72f0d04e9f21c4bf5c8"
 BASE_URL = "https://api.openweathermap.org/data/2.5/forecast"
 
 # Function to fetch weather data with better error handling
